7937162214f6a3728f381d258ada193429b0e3f7
gpelincel <gabrielpelincel@gmail.com>
2025-03-28 10:04:22 -0300

    Feat: Product card rendered by API

--- app/components/Products/Card.tsx
+++ app/components/Products/Card.tsx
@@ -1,12 +1,14 @@
 import "./products.css";
 
-export default function Card() {
+export default function Card(props:any) {
+	let product = props.product;
+
 	return (
 		<div className="card">
-			<img src="/assets/img/image-1.png" />
+			<img src={product.featured_image_url} />
 			<div className="card-content">
-				<h3>Lorem ipsum dolor</h3>
-				<p>Lorem ipsum dolor sit amet consectetur.</p>
+				<h3>{product.title.rendered}</h3>
+				<p>{product.content.text}</p>
 			</div>
 		</div>
 	);
--- app/components/Products/Products.tsx
+++ app/components/Products/Products.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import "./products.css";
+import { fetchAPI } from "~/services/APIService";
 
 export default function Products() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		try {
+			fetchAPI("posts",['per_page=4']).then((products) => {
+				setProducts(products);
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+
+	const cardsList = products.map((product: any) => (
+		<Card key={product.id} product={product}></Card>
+	));
+
 	return (
 		<div className="products">
 			<div className="products-container">
@@ -11,12 +29,9 @@ export default function Products() {
 					scelerisque quis commodo
 				</p>
 			</div>
-            <div className="card-list">
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-            </div>
+			<div className="card-list">
+				{cardsList}
+			</div>
 		</div>
 	);
 }
